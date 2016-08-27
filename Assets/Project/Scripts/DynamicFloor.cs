eb19eabb1d47be4f8331ee1d30ee003ac00f0f25
Taro Omiya <taro.omiya@omiyagames.com>
2016-08-27 17:02:39 -0400

    Attempting to create a dynamic graphic providing contour for the floor. Not doing well with the default line renderer.  Let's try something different.

---
+++ Assets/Project/Scripts/DynamicFloor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+[ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(EdgeCollider2D))]
+public class DynamicFloor : MonoBehaviour
+{
+    LineRenderer lineGraphic = null;
+    EdgeCollider2D lineCollider = null;
+
+    Vector2[] originalPoints = null;
+
+    public LineRenderer Graphic
+    {
+        get
+        {
+            if(lineGraphic == null)
+            {
+                lineGraphic = GetComponent<LineRenderer>();
+            }
+            return lineGraphic;
+        }
+    }
+
+    public EdgeCollider2D Collider
+    {
+        get
+        {
+            if(lineCollider == null)
+            {
+                lineCollider = GetComponent<EdgeCollider2D>();
+            }
+            return lineCollider;
+        }
+    }
+
+    void Start()
+    {
+        // Populate original points
+        originalPoints = new Vector2[Collider.pointCount];
+        for (int index = 0; index < Collider.pointCount; ++index)
+        {
+            originalPoints[index] = Collider.points[index];
+        }
+
+        // Update lineGraphics
+        UpdateGraphics();
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        UpdateGraphics();
+#else
+#endif
+    }
+
+    void UpdateGraphics()
+    {
+        Graphic.SetVertexCount(Collider.pointCount * 2 - 2);
+        int graphicIndex = 0;
+        for(int colliderIndex = 0; colliderIndex < Collider.pointCount; ++colliderIndex)
+        {
+            if((colliderIndex == 0) || (colliderIndex == (Collider.pointCount - 1)))
+            {
+                Graphic.SetPosition(graphicIndex, Collider.points[colliderIndex]);
+                ++graphicIndex;
+            }
+            else
+            {
+                Graphic.SetPosition(graphicIndex, Collider.points[colliderIndex]);
+                ++graphicIndex;
+                Graphic.SetPosition(graphicIndex, (Collider.points[colliderIndex] + (Random.insideUnitCircle * 0.01f)));
+                ++graphicIndex;
+            }
+        }
+    }
+}
