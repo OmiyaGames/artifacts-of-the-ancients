2b71ac7f00f51c51981f2996d644f84bf8524195
Taro Omiya <taro.omiya@omiyagames.com>
2016-08-28 14:15:36 -0400

    Working on adding on/off switches.

---
+++ Assets/Project/Scripts/IToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public abstract class IToggle : MonoBehaviour
+{
+    Animator animatorCache = null;
+
+    public abstract bool IsOn
+    {
+        get;
+        set;
+    }
+
+    public Animator CachedAnimator
+    {
+        get
+        {
+            if(animatorCache == null)
+            {
+                animatorCache = GetComponent<Animator>();
+            }
+            return animatorCache;
+        }
+    }
+
+    public void Toggle()
+    {
+        IsOn = !IsOn;
+    }
+}
