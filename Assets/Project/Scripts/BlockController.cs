7e98a22ce96ddd0df129e5188e1a678661980eb8
Taro Omiya <taro.omiya@omiyagames.com>
2016-08-28 21:43:37 -0400

    Working on all sorts of contraptions to make block pull-push working.

---
+++ Assets/Project/Scripts/BlockController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using OmiyaGames;
+
+[DisallowMultipleComponent]
+public class BlockController : MonoBehaviour
+{
+    [SerializeField]
+    SpringJoint2D boxJoint;
+
+    Block lastblock = null;
+
+    void Update()
+    {
+        if (StageState.Instance != null)
+        {
+            if ((CrossPlatformInputManager.GetButtonDown("Fire1") == true) &&
+                (StageState.Instance.IsPopUpVisible == true) &&
+                (StageState.Instance.Controller.IsCrouching == false) &&
+                (StageState.Instance.CurrentActionOnFire1 == ITriggers.Action.Grab))
+            {
+                SetCrouching((StageState.Instance.CurrentTriggerOnFire1 as Block), Block.GrabCondition.StartGrab);
+            }
+            else if (StageState.Instance.Controller.IsCrouching == true)
+            {
+                if (StageState.Instance.Platformer.IsGrounded == false)
+                {
+                    SetCrouching(null, Block.GrabCondition.AccidentLetGo);
+                }
+                else if (CrossPlatformInputManager.GetButtonUp("Fire1") == true)
+                {
+                    SetCrouching(null, Block.GrabCondition.ManualLetGo);
+                }
+            }
+        }
+    }
+
+    void SetCrouching(Block blockTrigger, Block.GrabCondition reason)
+    {
+        // Check if we wan to crough
+        bool flag = (blockTrigger != null);
+
+        // Update flags
+        boxJoint.gameObject.SetActive(flag);
+        StageState.Instance.Controller.IsCrouching = flag;
+
+        // make changes specific to flags
+        if (flag == true)
+        {
+            // If crouching, connect the rigidbody
+            boxJoint.connectedBody = blockTrigger.Body;
+
+            // Setup block
+            blockTrigger.SetupBlock(reason);
+            lastblock = blockTrigger;
+        }
+        else
+        {
+            // If not crouching anymore, disconnect
+            boxJoint.connectedBody = null;
+
+            // Setup block
+            if (lastblock != null)
+            {
+                lastblock.SetupBlock(reason);
+                lastblock = null;
+            }
+        }
+
+        // Setup joint
+        boxJoint.autoConfigureConnectedAnchor = !flag;
+        boxJoint.autoConfigureDistance = !flag;
+    }
+}
